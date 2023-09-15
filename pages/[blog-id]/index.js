0172acdbd0f9121ec62a7e6d21dc7e9e6801b87a
devjasim <jasimfbd@gmail.com>
2023-09-15 17:25:56 +0600

    added dashboard api

---
+++ pages/[blog-id]/index.js
@@ -0,0 +1,12 @@
+import AppLayoutComponent from '@/components/app/layout/AppLayout';
+import React from 'react';
+
+const BlogPage = () => {
+  return (
+    <AppLayoutComponent>
+      <h1>Blog page</h1>
+    </AppLayoutComponent>
+  );
+};
+
+export default BlogPage;
